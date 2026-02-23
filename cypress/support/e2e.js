90fde08945cf2b0fc3f9ee4c537601145b3d02dc
PeterBug <mac@MACs-MacBook-Pro.local>
2026-02-23 02:26:35 +0100

    Code refactor implementation

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,5 +1,18 @@
-// This file is processed and loaded automatically before your test files.
-// You can use this to import custom commands or global configuration.
-
+// cypress/support/e2e.js
 import './commands';
 
+beforeEach(() => {
+  // Stub out all analytics calls so they never appear in logs or slow down tests
+  cy.intercept('POST', '**/google-analytics.com/**', { statusCode: 204, body: '' }).as('blockGA');
+  cy.intercept('GET', '**/googletagmanager.com/**', { statusCode: 204, body: '' }).as('blockGTM');
+});
+
+Cypress.on('uncaught:exception', (err) => {
+  const ignoredErrors = [
+    'ResizeObserver loop limit exceeded',
+  ];
+
+  if (ignoredErrors.some((msg) => err.message.includes(msg))) {
+    return false;
+  }
+});
